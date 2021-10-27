f4b3aaf3bcf52a69e7a859ae50b478729bc58159
Ahmed-Mahgoub <ahmedelkarar9@gmail.com>
2021-10-27 12:48:22 -0700

    adding relevant user inputs to strealit interface

--- pychain.py
+++ pychain.py
@@ -169,20 +169,16 @@ pychain = setup()
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
-
+sender = st.text_input("Sender_ID")
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
-
+receiver = st.text_input("Receiver_ID")
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
-
+amount = st.text_input("Amount")
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
     prev_block_hash = prev_block.hash_block()
@@ -192,7 +188,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        record=Record,
         creator_id=42,
         prev_hash=prev_block_hash
     )
